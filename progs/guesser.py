a7e5824b510e8c531ab33c8119946052ddeba02d
Aditya <sp.aditya@gmail.com>
2018-09-20 16:14:09 +0530

    Add files via upload

--- progs/guesser.py
+++ progs/guesser.py
@@ -1,13 +1,16 @@
 import random
 r = random.randint(1,100)
-
+guesses = []
 print("I have thought of a number. Its your turn to guess.")
 while True:
     ui = int(input("Enter you guess[1,100] :"))
+    guesses.append(ui)
     if ui < r:
         print("Low")
     elif ui > r:
         print("High")
     else:
         print("You guessed it right!")
+        print("You took", len(guesses), "guesses")
+        print("These were your guesses: ", guesses)
         break
